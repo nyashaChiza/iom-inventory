8292edbeab824d283137f4bfaa0ecddf5b333b2b
Nyasha Chizampeni <nchizampeni@gmail.com>
2024-09-16 17:57:03 +0200

    updated accounts admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Role
 
-# Register your models here.
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created', 'updated')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'username', 'role', 'is_staff', 'is_active', 'created_at', 'updated_at')
+    list_filter = ('is_staff', 'is_active', 'role')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('role',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('role',)}),
+    )
+
+# Register the models with the admin site
+admin.site.register(Role, RoleAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
